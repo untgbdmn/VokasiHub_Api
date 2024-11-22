ba7df2d3aebcbb484367af1ad1b123c1b616c8fd
untgbdmn <budimanuntung14@gmail.com>
2024-11-22 21:30:10 +0700

    Konfigurasi Auth

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/register', RegisterController::class)->name('register');
+Route::post('/login', LoginController::class)->name('login');
+Route::post('/logout', LogoutController::class)->name('logout');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
